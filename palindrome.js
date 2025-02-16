3bec51266c9eeb8fc8614f7b8b9af9cc3023c345
PsYcO-Jass <jsingh354@mylangara.ca>
2025-02-15 18:01:45 -0800

    first commit

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+// src/palindrome.js
+function isPalindrome(str) {
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return cleaned === cleaned.split('').reverse().join('');
+  }
+  
+  module.exports = isPalindrome;
+  
